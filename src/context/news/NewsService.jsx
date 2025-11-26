4b6fd62ba06ac7e65fa743f6a928424d0fdec837
ShrutiGaud3 <shrutigaud3@gmail.com>
2025-11-26 18:22:01 +0530

    Update NewsService.jsx

--- src/context/news/NewsService.jsx
+++ src/context/news/NewsService.jsx
@@ -1,4 +1,5 @@
 export const fetchNews = async (topic) => {
+      const q = topic?.trim() || "breaking-news";
     const response = await fetch(`https://gnews.io/api/v4/search?q=${topic}&apikey=f41451d1f86e98907dc4d3de4fb633b1`)
     const data = await response.json()
      return data.articles
