f5fd2b7821a74383672dd78fc865ec3389ae10f8
ShrutiGaud3 <shrutigaud3@gmail.com>
2025-11-26 18:25:59 +0530

    Update NewsService.jsx

--- src/context/news/NewsService.jsx
+++ src/context/news/NewsService.jsx
@@ -1,7 +1,10 @@
-export const fetchNews = async (topic) => {
-      const q = topic?.trim() || "breaking-news";
-    const response = await fetch(`https://gnews.io/api/v4/search?q=${topic}&apikey=f41451d1f86e98907dc4d3de4fb633b1`)
-    const data = await response.json()
-     return data.articles
-    
-}
+const proxy = "https://api.allorigins.win/raw?url=";
+
+export const fetchNews = async (topic = "india") => {
+  const url = `https://gnews.io/api/v4/search?q=${topic}&apikey=f41451d1f86e98907dc4d3de4fb633b1`;
+
+  const response = await fetch(proxy + encodeURIComponent(url));
+
+  const data = await response.json();
+  return data.articles;
+};
